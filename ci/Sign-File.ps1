c00ffbfadfb22d19f39209f791ce82ee253fc0bb
Jakub Kocka <jakub.kocka@espressif.com>
2024-04-29 12:59:35 +0200

    fix: Uploading signed driver to release

--- ci/Sign-File.ps1
+++ ci/Sign-File.ps1
@@ -19,7 +19,7 @@ function FindSignTool {
     if (Test-Path -Path $SignTool -PathType Leaf) {
         return $SignTool
     }
-    $sdkVers = "10.0.22000.0", "10.0.20348.0", "10.0.19041.0", "10.0.17763.0"
+    $sdkVers = "10.0.22621.0", "10.0.22000.0", "10.0.20348.0", "10.0.19041.0", "10.0.17763.0"
     Foreach ($ver in $sdkVers)
     {
         $SignTool = "${env:ProgramFiles(x86)}\Windows Kits\10\bin\${ver}\x64\signtool.exe"
